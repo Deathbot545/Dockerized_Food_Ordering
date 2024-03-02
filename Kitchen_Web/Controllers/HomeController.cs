3ffd07c87f7008c2945df6867b3862788f23f291
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-03-02 15:08:39 +0530

    jk

---
+++ Kitchen_Web/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Core.DTO;
+using Core.Services.Orderser;
+using Core.Services.OutletSer;
+using Core.ViewModels;
+using Kitchen_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Kitchen_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOutletService _outletService;
+        private readonly IOrderService _orderService; // 
+
+        public HomeController(ILogger<HomeController> logger, IOutletService outletService, IOrderService orderService)
+        {
+            _logger = logger;
+            _outletService = outletService;
+            _orderService = orderService;
+        }
+
+      
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Index(int? outletId)
+        {
+            if (outletId.HasValue)
+            {
+                var tables = _outletService.GetTablesByOutlet(outletId.Value);
+                List<OrderDTO> orders;
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync($"https://restosolutionssaas.com:7268/api/OrderApi/GetOrdersForOutlet/{outletId.Value}");
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var errorResponse = await response.Content.ReadAsStringAsync();
+                        return View("Error", new ErrorViewModel { Message = $"API Error: {errorResponse}" });
+                    }
+                    try
+                    {
+                        orders = await response.Content.ReadAsAsync<List<OrderDTO>>();
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($"Error while processing the API response: {ex.Message}");
+                        return View("Error", new ErrorViewModel { Message = $"API Processing Error: {ex.Message}" });
+                    }
+                    var model = new OutletViewModel { Tables = tables, Orders = orders };
+                    return View("Index", model); // Simplify the view path
+                }
+            }
+            else
+            {
+                // If no outletId is provided, just return the default view without additional data.
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Kitchen_Web/Program.cs
@@ -0,0 +1,118 @@
+using Core.Services.Orderser;
+using Core.Services.OutletSer;
+using Infrastructure.Data;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+builder.Services.AddControllersWithViews(); // This line is crucial
+// Inside ConfigureKestrel method
+var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    serverOptions.ListenAnyIP(80); // Listen for HTTP
+    serverOptions.ListenAnyIP(443, listenOptions =>
+    {
+        try
+        {
+            listenOptions.UseHttps("/etc/ssl/certs/certificate.pfx", "raaed");
+            logger.LogInformation("HTTPS setup was successful.");
+        }
+        catch (Exception ex)
+        {
+            // Log detailed information about the exception
+            logger.LogError(ex, "An error occurred while setting up HTTPS.");
+        }
+    });
+});
+
+// Inside ConfigureServices method or directly in the Program.cs
+var dataProtectionKeysPath = "/root/.aspnet/DataProtection-Keys"; // Path inside the container
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(dataProtectionKeysPath));
+// Register the HttpClient, set its base address, and configure the timeout
+builder.Services.AddHttpClient("namedClient", c =>
+{
+    c.BaseAddress = new Uri(configuration["ApiBaseUrl"]);
+    c.Timeout = TimeSpan.FromSeconds(200); // Set the desired timeout here
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+});
+
+builder.Services.AddSignalR();
+
+builder.Services.AddScoped<IOutletService, OutletService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.Lax;
+    options.HttpOnly = HttpOnlyPolicy.Always;
+    options.Secure = CookieSecurePolicy.SameAsRequest; // Important for HTTP
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "your-authority-here";  // e.g., https://your-auth-server.com/
+        options.Audience = "your-audience-here";    // e.g., your-client-id
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // Additional token validation parameters here
+        };
+    })
+    .AddGoogle(options =>
+    {
+        options.ClientId = configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+    });
+
+
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+builder.Configuration.AddJsonFile("Kitchen_Web_appsettings.json", optional: true, reloadOnChange: true);
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment() || builder.Configuration.GetValue<bool>("ShowDetailedErrors"))
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();

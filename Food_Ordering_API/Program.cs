42a7ff14281d73350fcda2df079a48bb730888e5
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-04-05 09:56:03 +0530

    somehig

--- Food_Ordering_API/Program.cs
+++ Food_Ordering_API/Program.cs
@@ -21,7 +21,7 @@ builder.WebHost.ConfigureKestrel(serverOptions =>
         listenOptions.UseHttps("/etc/ssl/certs/certificate.pfx", "raaed");
     });
 });
-
+//hhh
 // Add services to the container.
 ConfigureDatabase(builder);
 ConfigureIdentity(builder);

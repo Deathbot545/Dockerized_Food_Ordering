bd13a8bda33d98b5fc01961ca4b41bfdda34fdf4
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-04-05 08:20:18 +0530

    A descriptive message about your changes

--- Food_Ordering_Web/Program.cs
+++ Food_Ordering_Web/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddHttpClient("namedClient", c =>
 });
 //builder.Services.AddScoped<AccountService, AccountService>();
 builder.Services.AddSignalR();
-
+//test
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {

eb418ae67cfd597e69be7f514c907a93812f3bb0
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-04-05 10:05:38 +0530

    somehig

--- Food_Ordering_Web/Program.cs
+++ Food_Ordering_Web/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddHttpClient("namedClient", c =>
 });
 //builder.Services.AddScoped<AccountService, AccountService>();
 builder.Services.AddSignalR();
-//jk
+//j
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {

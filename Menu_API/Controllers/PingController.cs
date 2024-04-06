006e342f000e2e59ca0df9759871278a33dfe666
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-04-06 14:50:23 +0530

    Menu_API

---
+++ Menu_API/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Menu_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is up and running.");
+        }
+    }
+}

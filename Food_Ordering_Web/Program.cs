c5f296cd821be26bb0096605889a333a68b6fc91
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-04-12 08:39:45 +0530

    Cookie Authentication

--- Food_Ordering_Web/Program.cs
+++ Food_Ordering_Web/Program.cs
@@ -43,21 +43,6 @@ builder.Services.AddHttpClient("namedClient", c =>
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = configuration["Jwt:Issuer"],
-        ValidAudience = configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
-    };
 })
 .AddCookie(options => // This is where you set the cookie options for authentication cookies
 {

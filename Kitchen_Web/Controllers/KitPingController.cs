c0e2576c229c6d80d959d416c55164bf921adc76
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2024-04-16 08:28:01 +0530

    Kitchen Web

---
+++ Kitchen_Web/Controllers/KitPingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kitchen_Web.Controllers
+{
+
+    [Route("kitchen/[controller]")]
+    public class KitPingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is up and running.");
+        }
+    }
+}

0ba85c8b855b4d35d7a21ba41da801caa46aa1ae
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-05-14 08:02:29 +0530

    Kitchen Home Constroller Update

--- Kitchen_Web/Controllers/HomeController.cs
+++ Kitchen_Web/Controllers/HomeController.cs
@@ -36,36 +36,7 @@ namespace Kitchen_Web.Controllers
 
         public async Task<IActionResult> Index(int? outletId)
         {
-            if (outletId.HasValue)
-            {
-                var tables = _outletService.GetTablesByOutlet(outletId.Value);
-                List<OrderDTO> orders;
-                using (var httpClient = new HttpClient())
-                {
-                    var response = await httpClient.GetAsync($"https://restosolutionssaas.com/api/OrderApi/GetOrdersForOutlet/{outletId.Value}");
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        var errorResponse = await response.Content.ReadAsStringAsync();
-                        return View("Error", new ErrorViewModel { Message = $"API Error: {errorResponse}" });
-                    }
-                    try
-                    {
-                        orders = await response.Content.ReadAsAsync<List<OrderDTO>>();
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError($"Error while processing the API response: {ex.Message}");
-                        return View("Error", new ErrorViewModel { Message = $"API Processing Error: {ex.Message}" });
-                    }
-                    var model = new OutletViewModel { Tables = tables, Orders = orders };
-                    return View("Index", model); // Simplify the view path
-                }
-            }
-            else
-            {
-                // If no outletId is provided, just return the default view without additional data.
-                return View();
-            }
+            return View("");
         }
 
     }

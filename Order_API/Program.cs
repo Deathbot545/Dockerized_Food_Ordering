c2feb51237c451cac971c0fa295e9d84f0a633c4
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-05-23 18:45:52 +0530

    Update Program.cs

--- Order_API/Program.cs
+++ Order_API/Program.cs
@@ -18,7 +18,7 @@ builder.WebHost.ConfigureKestrel((context, serverOptions) =>
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
-// SignalR 
+// Signa
 builder.Services.AddSignalR();
 
 builder.Services.AddControllers();

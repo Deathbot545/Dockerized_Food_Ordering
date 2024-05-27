d48085aaf5b57fbf5eff6ede9917e20489cf76c1
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-05-27 17:28:04 +0530

    Menu Front End Update

---
+++ Menu_API/Migrations/20240527112741_AddSizeToOrderDetail.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Menu_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSizeToOrderDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegetarian",
+                table: "MenuItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "MenuItemsSize",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Size = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    MenuItemId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItemsSize", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuItemsSize_MenuItems_MenuItemId",
+                        column: x => x.MenuItemId,
+                        principalTable: "MenuItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItemsSize_MenuItemId",
+                table: "MenuItemsSize",
+                column: "MenuItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItemsSize");
+
+            migrationBuilder.DropColumn(
+                name: "IsVegetarian",
+                table: "MenuItems");
+        }
+    }
+}

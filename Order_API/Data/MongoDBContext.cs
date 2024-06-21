6bc8819bc2a7f82bf336fa732ee21e1a17c57dab
Raaed Bucksimiar <85855492+Deathbot545@users.noreply.github.com>
2024-06-21 09:16:37 +0530

    Documnt DB for Orders

--- Order_API/Data/MongoDBContext.cs
+++ Order_API/Data/MongoDBContext.cs
@@ -7,13 +7,21 @@ namespace Order_API.Data
     public class MongoDBContext
     {
         private readonly IMongoDatabase _database = null;
+        private readonly ILogger<MongoDBContext> _logger;
 
-        public MongoDBContext(IOptions<MongoDBSettings> settings)
+        public MongoDBContext(IOptions<MongoDBSettings> settings, ILogger<MongoDBContext> logger)
         {
+            _logger = logger;
+
             var client = new MongoClient(settings.Value.ConnectionString);
             if (client != null)
             {
+                _logger.LogInformation("MongoDB connection successful.");
                 _database = client.GetDatabase(settings.Value.DatabaseName);
+            }
+            else
+            {
+                _logger.LogError("MongoDB connection failed.");
             }
         }
 
